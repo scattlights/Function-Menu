6dff7169a4f17b517fc8bb47b5f63f2294f7c15b
just2so <169085685+just2so@users.noreply.github.com>
2024-06-08 12:54:38 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,10 @@
-#！/bin/bash
+#!/bin/bash
 FILE="/test/one.txt"
 DIRECTORY="/test"
 if [[ -d "$DIRECTORY" && -f "$FILE" ]]; then
-	echo "This is a test" >> "$FILE"
+    echo "This is a test" >> "$FILE"
 else
-	echo "目录或文件不存在！"
-fi
+    echo "目录或文件不存在！"
+fi  # 这里是 if 结构的结束符号
 
 exit 0
-	
-	
-	
-
