d1f75b24ce795b4c75598772a338bd947d9f7bca
just2so <169085685+just2so@users.noreply.github.com>
2024-06-08 18:58:49 +0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 # 定义颜色代码
-RED='\033[0;31m'
 GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
 NC='\033[0m' # 无颜色
 
 #需要清除垃圾文件的目录
-read -p "$(echo -e ${RED}输入需要清除垃圾文件的目录:${NC}) " directory
+read -p "$(echo -e ${YELLOW}输入需要清除垃圾文件的目录:${NC}) " directory
 
 # 清理临时文件
-echo "清理临时文件..."
+echo -e "${GREEN}清除临时文件...${NC}"
 find "$directory" -type f -name "*.tmp" -exec rm -f {} \;
 
 # 清理日志文件
-echo "清理日志文件..."
+echo -e "${GREEN}清除日志文件...${NC}"
 find "$directory" -type f -name "*.log" -exec rm -f {} \;
 
-echo "垃圾文件清理完成!"
+echo -e "${GREEN}垃圾清除完成...${NC}"
 
 	
 	
