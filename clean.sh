d6d84d692cf08d124ed3ba54160a87cdd2b9d230
just2so <169085685+just2so@users.noreply.github.com>
2024-06-08 18:53:14 +0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -5,7 +5,7 @@ GREEN='\033[0;32m'
 NC='\033[0m' # 无颜色
 
 #需要清除垃圾文件的目录
-read -p "(echo -e ${RED}输入需要清除垃圾文件的目录:${NC}) " directory
+read -p "$(echo -e ${RED}输入需要清除垃圾文件的目录:${NC}) " directory
 
 # 清理临时文件
 echo "清理临时文件..."
