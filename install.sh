783c7f9fa5a7ff65782945942f44e446b1b888cb
just2so <169085685+just2so@users.noreply.github.com>
2024-06-08 14:23:11 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 FILE="/test/one.txt"
 DIRECTORY="/test"
 if [[ -d "$DIRECTORY" && -f "$FILE" ]]; then
-	echo "This is a test!" >> "$FILE"
+	echo "$(date): This is a test!" >> "$FILE"
+ 	echo "Execution complete!"
 else
 	echo "The directory or file does not exist!"
 fi
