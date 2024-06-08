611d403b2514e3857cbf1e386a3bbf52a33bd85a
just2so <169085685+just2so@users.noreply.github.com>
2024-06-08 12:39:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #！/bin/bash
 FILE="/test/one.txt"
 DIRECTORY="/test"
-IF [[ -d "$DIRECTORY" && -f "$FILE" ]]; then
+if [[ -d "$DIRECTORY" && -f "$FILE" ]]; then
 	echo "This is a test" >> one.txt
 else
 	echo "目录或文件不存在！"
