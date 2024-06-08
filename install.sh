595f44d5e227bec41b4a987d5264281ac715b592
just2so <169085685+just2so@users.noreply.github.com>
2024-06-08 14:03:38 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 FILE="/test/one.txt"
 DIRECTORY="/test"
 if [[ -d "$DIRECTORY" && -f "$FILE" ]]; then
-	echo "This is a test" >> "$FILE"
+	echo "This is a test!" >> "$FILE"
 else
 	echo "目录或文件不存在！"
 fi
